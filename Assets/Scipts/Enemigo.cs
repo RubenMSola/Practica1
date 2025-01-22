dc2dd8ed71a447fdbb22ec88d7c7100907b9689b
RubenMSola <RUBY310504@GMAIL.COM>
2025-01-22 20:58:43 +0100

    Empezar el NavMesh

--- Assets/Scipts/Enemigo.cs
+++ Assets/Scipts/Enemigo.cs
@@ -1,9 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Enemigo : MonoBehaviour
 {
+    public Transform PatrolRoute;
+    public List<Transform> Locations;
+    private int _locationIndex = 0;
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
+
+    void Update()
+    {
+        if (_agent.remainingDistance < 0.2f && !_agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        } 
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach (Transform child in PatrolRoute)
+        {
+            Locations.Add(child);
+        }
+    }
+    void MoveToNextPatrolLocation()
+    {
+        if (Locations.Count == 0)
+            return;
+        _agent.destination = Locations[_locationIndex].position;
+        _locationIndex = (_locationIndex +1 ) % Locations.Count;
+    }
     private void OnTriggerEnter(Collider other) //Creamos un metodo para que detecte la collision trigger de un objeto y si interactua con otro objeto "Personaje" que pase algo "lo muestre en la comsola"
     {
         if(other.name == "Personaje")
